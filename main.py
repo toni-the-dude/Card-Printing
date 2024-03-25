54f32b2ddff6b60f16e02e0218fcb633eac6a6b7
Ștefan Emil - Antoniu <76394495+toni-the-dude@users.noreply.github.com>
2024-03-25 16:36:56 +0000

    Add multiple copies and change rows

--- main.py
+++ main.py
@@ -11,25 +11,39 @@ canvas = Image.new("RGB", (794, 1123), (255, 255, 255)) # Create blank A4 canvas
 # canvas.save("empty-canvas.png") # Save it for testing
 print(os.listdir(images)) # Verifying "images" folder contents
 
-location = (0, 0) # Coordinates
+offset_x = 0
+offset_y = 0
+location = (offset_x, offset_y) # Coordinates
+cards_per_row = 3
 
 for image in os.listdir(images): # Parsing contents of "images"
 
     filename = "./images/" + image # Provide full path for safety sake
-    
-    # Update location for next image
+    copies = int(input("How many copies of {0} do you want?\n".format(image))) # How many of the same image to print
+
+    while copies < 1 or copies > 9:
+        copies = int(input("Invalid input. Try 1-9.\n"))
 
     with Image.open(filename) as img:
 
-        img.load
+        while copies != 0:
 
-        canvas.paste(
-            img.resize((240, 336)), # Image being pasted in also gets resized
-            location # Where to place top-left corner of the image
-        )
+            img.load
 
-        location = (location[0] + 240, location[1])
+            canvas.paste(
+                img.resize((240, 336)), # Image being pasted in also gets resized
+                location # Where to place top-left corner of the image
+            )
 
-        # print(type(img), img.format, img.size)
+            if cards_per_row == 1: # Last card for current row
+                location = (offset_x, location[1] + 336) # Go to the next row within the canvas
+                cards_per_row = 3
+            else:
+                location = (location[0] + 240, location[1]) # Update location for next image
+                cards_per_row -= 1
+
+            copies -= 1
+
+            # print(type(img), img.format, img.size)
 
 canvas.save("DLC.png") # Save final product
